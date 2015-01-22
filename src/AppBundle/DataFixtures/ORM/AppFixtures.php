1e13f8f34eb7b76da29572ce355b1fb6b62882b1
Leanna <leanna@knplabs.com>
2015-01-22 12:39:25 -0500

    fortune machine

--- src/AppBundle/DataFixtures/ORM/AppFixtures.php
+++ src/AppBundle/DataFixtures/ORM/AppFixtures.php
@@ -27,22 +27,38 @@ class AppFixtures extends DataFixtureLoader
 
     private $fortunes = [
         'job' => [
-            'FOO'
+            'It would be best to maintain a low profile for now.',
+            '404 Fortune not found. Abort, Retry, Ignore?',
+            'You laugh now, wait til you get home.',
+            'If your work is not finished, blame it on the computer.',
         ],
         'lunch' => [
-            'BAR'
+            'You will be hungry again in one hour.',
+            'Vampires will soon strike you if you do not order again',
+            'A nice cake is waiting for you',
+            'Warning: Do not eat your fortune',
         ],
         'proverb' => [
-            'BOB'
+            'A conclusion is simply the place where you got tired of thinking.',
+            'Cookie said: "You really crack me up"',
+            'When you squeeze an orange, orange juice comes out. Because that\'s what\'s inside.',
+            ''
         ],
         'pets' => [
-            'PET'
+            'There\'s no such thing as an ordinary cat',
+            'That wasn\'t chicken',
         ],
         'love' => [
-            'LOVE'
+            'An alien of some sort will be appearing to you shortly!',
+            'Are your legs tired? You\'ve been running through someone\'s mind all day long.',
+            'run',
         ],
         'lucky_number' => [
-            42
+            42,
+            12,
+            '10^2',
+            'Jar Jar Binks',
+            'Pi',
         ]
     ];
 }
