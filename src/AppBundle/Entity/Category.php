c490ca7cb9f36a12b0e1e2aabf29a2ad98207690
Ryan Weaver <ryan@thatsquality.com>
2015-01-22 10:31:05 -0500

    Initial entities

---
+++ src/AppBundle/Entity/Category.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="iconKey", type="string", length=20)
+     */
+    private $iconKey;
+
+    /**
+     * @ORM\OneToMany(targetEntity="FortuneCookie", mappedBy="category")
+     */
+    private $fortuneCookies;
+
+    public function __construct()
+    {
+        $this->fortuneCookies = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set iconKey
+     *
+     * @param string $iconKey
+     * @return Category
+     */
+    public function setIconKey($iconKey)
+    {
+        $this->iconKey = $iconKey;
+
+        return $this;
+    }
+
+    /**
+     * Get iconKey
+     *
+     * @return string 
+     */
+    public function getIconKey()
+    {
+        return $this->iconKey;
+    }
+
+    /**
+     * @return ArrayCollection|FortuneCookie[]
+     */
+    public function getFortuneCookies()
+    {
+        return $this->fortuneCookies;
+    }
+}
---
+++ src/AppBundle/Entity/FortuneCookie.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * FortuneCookie
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\FortuneCookieRepository")
+ */
+class FortuneCookie
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var Category
+     *
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy="fortuneCookies")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fortune", type="string", length=255)
+     */
+    private $fortune;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="numberPrinted", type="integer")
+     */
+    private $numberPrinted;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fortune
+     *
+     * @param string $fortune
+     * @return FortuneCookie
+     */
+    public function setFortune($fortune)
+    {
+        $this->fortune = $fortune;
+
+        return $this;
+    }
+
+    /**
+     * Get fortune
+     *
+     * @return string 
+     */
+    public function getFortune()
+    {
+        return $this->fortune;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return FortuneCookie
+     */
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set numberPrinted
+     *
+     * @param integer $numberPrinted
+     * @return FortuneCookie
+     */
+    public function setNumberPrinted($numberPrinted)
+    {
+        $this->numberPrinted = $numberPrinted;
+
+        return $this;
+    }
+
+    /**
+     * Get numberPrinted
+     *
+     * @return integer 
+     */
+    public function getNumberPrinted()
+    {
+        return $this->numberPrinted;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param mixed $category
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+    }
+}
